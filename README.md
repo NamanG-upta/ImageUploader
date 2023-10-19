841ed3b8b08c0187e6fb5c8c1bfa62c8b1e8d105
Naman Gupta <nmngupta@amazon.com>
2023-10-19 19:15:39 +0530

    updated READ me and removed unnecessary imports

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# ImageUploader
-REST APIs to perfrom CRUD operation on images
+## Spring Boot File Upload / Download Rest API Example
+
+## Steps to Setup
+
+
+**1. Run the app using maven**
+
+```bash
+cd file-storage-service
+mvn clean install
+
+Note: Ensure you are using java 11
+```
+
+**2. Setting up database**
+
+    1. Log in into sql. You can open mySqlWorkBench also.
+    2. Run command: create database codecademy;
+
+**3. Running the application
+
+    1. Open StorageServiceApplication file. And run the file.
+
+That's it! The application can be accessed at `http://localhost:8000`.
--- src/main/java/com/codecademyAssignment/StorageServiceApplication.java
+++ src/main/java/com/codecademyAssignment/StorageServiceApplication.java
@@ -1,16 +1,7 @@
 package com.codecademyAssignment;
 
-import com.codecademyAssignment.service.ImageStorageService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class StorageServiceApplication {
